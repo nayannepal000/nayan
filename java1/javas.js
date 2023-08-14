ca6eff2ade3e4835e78b6b4aba55464ff5cc7643
NayanNepal <138751317+nayannepal000@users.noreply.github.com>
2023-08-13 22:11:09 -0700

    Add files via upload

---
+++ java1/javas.js
@@ -0,0 +1,11 @@
+function printhello(){
+    // document.getElementById("demo").innerHTML="hello"
+    // window.alert('hello world');
+    // console.log('hello bro');
+    let a=5000;
+    let b=6;
+    // let c=(a+b)*5;
+    let f =(15/100);
+    let d=a*f;
+    document.getElementById("demo").innerHTML=c;
+}
